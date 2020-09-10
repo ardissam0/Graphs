e7323ade44b7661b5f9b85664b6405197484481e
Sam Ardis <ardis.productions@gmail.com>
2020-09-10 18:19:31 -0400

    worked on adventure.py, I believe I got it working, MVP, sprint?!

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -10,11 +10,11 @@ world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
-# map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
-# map_file = "maps/test_loop.txt"
-# map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "maps/test_line.txt"
+map_file = "maps/test_cross.txt"
+map_file = "maps/test_loop.txt"
+map_file = "maps/test_loop_fork.txt"
+map_file = "projects/adventure/maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,7 +29,45 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+#reverse the direction
+oposite_directions = {'n': 's', 's': 'n', 'e': 'w', 'w': 'e'}
 
+
+# UPER:
+# set up directions for plater to follow
+# loop through all exits
+# set up visited rooms to turn around
+# if the room has not been visited then add to new path
+# update using recursion
+# return path
+# fill the traversal path with bfs function
+
+def bfs(starting_room, visited=set()):
+    # track new path
+    path =[]
+
+    # loop through exits
+    # use get exits in room.py
+    for direction in player.current_room.get_exits():
+        player.travel(direction)
+        # if the player is in visited room
+        if player.current_room in visited:
+            # then turn to another direction
+            player.travel(oposite_directions[direction])
+        # otherwise if it has not been visited
+        else:
+            # add to new path
+            visited.add(player.current_room)
+            path.append(direction)
+            # update using recursion
+            path = path + bfs(player.current_room, visited)
+            player.travel(oposite_directions[direction])
+            path.append(oposite_directions[direction])
+    # return the path
+    return path
+
+#fill in list with bfs current position
+traversal_path = bfs(player.current_room)
 
 # TRAVERSAL TEST
 visited_rooms = set()
@@ -60,3 +98,19 @@ while True:
         break
     else:
         print("I did not understand that command.")
+
+# set a Queue for the bfs function
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self, value):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
